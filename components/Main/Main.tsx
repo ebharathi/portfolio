a29efd8428dfdffa749fc24fa6edf55c51b51659
ebharathi <elayabharathi10102002@gmail.com>
2023-10-13 14:22:38 +0530

    changed pixels adjusted size

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -5,9 +5,9 @@ const Main=()=>{
     return (
        <div className="flex">
                 <div className="flex-[2]"></div>
-                <div className="flex-[4] mt-20 flex flex-row mx-4 md:mx-16 lg:mx-24">
+                <div className="flex-[3] mt-20 flex flex-row mx-2">
                     <div className="pt-20 flex-1 flex justify-center items-center">
-                            <img style={{objectFit:'cover'}} className="rounded-full w-300 h-300 lg:w-400 lg:h-350" src="https://res.cloudinary.com/dommky0uu/image/upload/v1697030549/1676809451987_yfc1kb.jpg"/>
+                            <img style={{objectFit:'cover'}} className="rounded-full w-300 h-300 lg:w-400 lg:h-400" src="https://res.cloudinary.com/dommky0uu/image/upload/v1697030549/1676809451987_yfc1kb.jpg"/>
                     </div>
                     <div className="flex-1">
                           <Content/>
