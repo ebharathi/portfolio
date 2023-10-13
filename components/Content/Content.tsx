16a6c9d97282f8edb7ced4f848a66644bce80106
ebharathi <elayabharathi10102002@gmail.com>
2023-10-13 11:40:55 +0530

    added design inside circle

--- components/Content/Content.tsx
+++ components/Content/Content.tsx
@@ -20,15 +20,15 @@ const Content=()=>{
              As a professional web developer, I'm passionate about creating exceptional digital experiences that help businesses achieve their goals. I'm proficient in all aspects of web development and have an impressive portfolio that showcases my abilities. From startups to large corporations, I have the skills and knowledge to elevate any digital project.
           </span>
        </div>
-       <div className='grid grid-cols-3 mt-1'>
+       <div className={`grid grid-cols-3 mt-1 ${sil.className}`}>
           <div className='flex justify-center items-center'>
-            <span className='w-100 h-100 bg-yellow-500 rounded-full'></span>   
+            <span className='w-100 h-100 bg-yellow-500 rounded-full cursor-pointer flex justify-center items-center text-white hover:bg-yellow-700 transition bg-500 ease-in-out'>PROJECTS</span>   
           </div>
           <div className='flex justify-center items-center'>
-            <span className='w-100 h-100 bg-blue-400 rounded-full'></span> 
+            <span className='w-100 h-100 bg-blue-400 rounded-full cursor-pointer flex justify-center items-center hover:bg-blue-700 transition bg-500 ease-in-out'>GITHUB</span> 
           </div>
           <div className='flex justify-center items-center'>
-          <span className='w-100 h-100 bg-red-300 rounded-full'></span> 
+            <span className='w-100 h-100 bg-red-300 rounded-full cursor-pointer flex justify-center items-center text-[#4d4a4a]  hover:bg-red-700 transition bg-500 ease-in-out'>RESUME</span> 
           </div>
        </div>
     </div>
