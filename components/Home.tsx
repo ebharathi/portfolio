30bab3a9a568c7a7e9ac8f79e5e5f23eeb668e30
ebharathi <elayabharathi10102002@gmail.com>
2023-10-26 00:46:44 +0530

    portfolio-design-update

--- components/Home.tsx
+++ components/Home.tsx
@@ -8,7 +8,32 @@ const HomeComponent=()=>{
     return (
         <div id="home" className="text-center flex justify-center items-center">
             <div>
-                <h1 className="text-[50px] pt-40">Hello, Im Elayabharathi,</h1>
+                <h1 className="text-[50px] pt-40">
+                    <span className="span">H</span>
+                    <span className="span">E</span>
+                    <span className="span">L</span>
+                    <span className="span">L</span>
+                    <span className="span">O</span>
+                    <span className="span">,</span>
+                    <span className="span-space"> </span>
+                    <span className="span">I</span>
+                    <span className="span">'</span>
+                    <span className="span">M</span>
+                    <span className="span-space"> </span>
+                    <span className="span">E</span>
+                    <span className="span">L</span>
+                    <span className="span">A</span>
+                    <span className="span">Y</span>
+                    <span className="span">A</span>
+                    <span className="span">B</span>
+                    <span className="span">H</span>
+                    <span className="span">A</span>
+                    <span className="span">R</span>
+                    <span className="span">A</span>
+                    <span className="span">T</span>
+                    <span className="span">H</span>
+                    <span className="span">I</span>
+                </h1>
                 <p>Portfolio currently under development</p>
                 <div className="flex justify-center items-center mt-80">
                 <span className="border-1 rounded-full p-3 hover:border-[#DE5E60] cursor-pointer" onClick={scollDown}>
