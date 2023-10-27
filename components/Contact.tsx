434ce4438979529d57a802012639b60e8a015329
ebharathi <elayabharathi10102002@gmail.com>
2023-10-27 23:52:42 +0530

    added contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,7 +1,23 @@
 const Contact=()=>{
     return (
-        <div>
-            contact
+        <div  className="bg-[#2F3238] px-60">
+             <div className="text-center pt-32">
+                <span className="text-[30px] font-bold text-white bottom-border">Get in Touch</span>
+            </div>
+            <div id="contact" className="text-center">
+    <div className="container">
+        <div className="address">Want to get in touch with me?  just feel free to drop me a line anytime. </div>
+        <div id="contact-form">
+            <div id="message"></div>
+            <form method="post" action="" name="contactform" id="contactform">
+                <input name="name" type="text" id="name"  className="inputForm2" placeholder="Name" / >
+                <input name="email" type="text" id="email" placeholder="Email"/>
+                <textarea name="comments"  id="comments" placeholder="Message"  ></textarea>
+                <input type="submit" className="send_message transition" id="submit" value="Send Message" />
+            </form>
+        </div>
+    </div>
+</div>
         </div>
     )
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import HomeComponent from '@/components/Home'
 import About from '@/components/About'
 import { useState } from 'react'
 import Projects from '@/components/Projects'
+import Contact from '@/components/Contact'
 export default function Home() {
    const [bg,setBG]=useState(false);
   return (
@@ -17,7 +18,8 @@ export default function Home() {
         <HomeComponent/>
        </div>
        <About/>
-       <Projects/>
+       {/* <Projects/> */}
+       <Contact/>
      </>
   )
 }
